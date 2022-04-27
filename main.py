e82da51d468e130d0886e75d8591d20d54350fde
Zachary352 <104509912+Zachary352@users.noreply.github.com>
2022-04-27 10:58:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,15 @@
 # Project: Countdown Timer using Github
 # File: countdownClass --> main.py
 #-----------------------------------------------------------------
+import time 
+'''The timercountdown function is used to import the userTime parameter. This will take the seconds and translate it to a timer to allow the user to countdown.'''
+userTime = input('Please enter the time in seconds: \n')
+def timeCountdown(userTime):
+    while userTime:
+        min, secs = divmod(userTime, 60)
+        timer = '{:02d} : {:02d}'.format(min, secs)
+        print('\r', timer, end = ' ')
+        time.sleep(1)
+        userTime -= 1
+    print('\n Timer Completed')
+timeCountdown(int(userTime))
